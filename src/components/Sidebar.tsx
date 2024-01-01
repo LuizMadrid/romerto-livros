d233ea21119e9f898570aba502831393f56c9dc5
Luiz Madrid <luizmadrid.dev@gmail.com>
2024-01-01 19:00:14 -0300

    navbar has been created.

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -5,131 +5,131 @@ import Image from 'next/image';
 import Button from './Button';
 import { Separator } from './ui/separator';
 
-import { AiFillSetting, AiFillHome } from 'react-icons/ai'
-import { ImBooks } from "react-icons/im";
-import { FaUser } from "react-icons/fa";
-import { IoInformationCircleOutline } from "react-icons/io5";
+import { AiFillSetting, AiFillHome } from 'react-icons/ai';
+import { ImBooks } from 'react-icons/im';
+import { FaUser } from 'react-icons/fa';
+import { IoInformationCircleOutline } from 'react-icons/io5';
 
 const Sidebar = () => {
-  return (
-    <>
-      <div className='h-screen flex justify-center items-start group/sidebar'>
+	return (
+		<>
+			<div className='h-screen flex justify-center items-start group/sidebar'>
 
-        <div className='h-full bg-white dark:bg-primaryDark text-base flex flex-col justify-between'>
-          <ul className='text-gray-500 dark:text-gray-300 font-bold w-[60px] group-hover/sidebar:w-full flex flex-col gap-4 p-2'>
+				<div className='h-full bg-white dark:bg-primaryDark text-base flex flex-col justify-between absolute top-0 left-0 group-hover/sidebar:absolute group-hover/sidebar:top-0 group-hover/sidebar:left-0'>
+					<ul className='text-gray-500 dark:text-gray-300 font-bold w-[60px] group-hover/sidebar:w-full flex flex-col gap-4 p-2'>
             
-            <Link
-              href={'/'}
-              className='flex justify-center items-center pt-2'>
-              <Image 
-                className='hidden group-hover/sidebar:block'
-                src="/Logo.png" 
-                height={140} 
-                width={140} 
-                alt='Website Logo'
-              />
-              <Image 
-                className='block group-hover/sidebar:hidden'
-                src="/Logo Closed.png" 
-                height={36} 
-                width={36} 
-                alt='Website Logo'
-              />
+						<Link
+							href={'/'}
+							className='flex justify-center items-center pt-2'>
+							<Image 
+								className='hidden group-hover/sidebar:block'
+								src="/Logo.png" 
+								height={140} 
+								width={140} 
+								alt='Website Logo'
+							/>
+							<Image 
+								className='block group-hover/sidebar:hidden'
+								src="/Logo Closed.png" 
+								height={36} 
+								width={36} 
+								alt='Website Logo'
+							/>
 
-            </Link>
+						</Link>
             
-            <Separator className='bg-primaryGray' />
+						<Separator className='bg-primaryGray' />
 
-            <li className='group my-[0.5px] group-hover/sidebar:my-[0.3px] group-hover/sidebar:-mt-[0.2px]'>
-              <Link 
-                href={'/'}>
+						<li className='group my-[0.5px] group-hover/sidebar:my-[0.3px] group-hover/sidebar:-mt-[0.2px]'>
+							<Link 
+								href={'/'}>
 
-                <Button variant='sidebar'className='block group-hover/sidebar:hidden'>
-                  <AiFillHome className='text-2xl group-hover:text-primary dark:group-hover:text-primaryLighter' />
-                </Button>
+								<Button variant='sidebar' className='block group-hover/sidebar:hidden'>
+									<AiFillHome className='text-2xl group-hover:text-primary dark:group-hover:text-primaryLighter' />
+								</Button>
 
-                <Button variant='sidebar' className='hidden group-hover/sidebar:flex'>
-                  <AiFillHome className='text-2xl group-hover:text-primary dark:group-hover:text-primaryLighter' />
-                  <span>Página Inicial</span>
-                </Button>
+								<Button variant='sidebar' className='hidden group-hover/sidebar:flex'>
+									<AiFillHome className='text-2xl group-hover:text-primary dark:group-hover:text-primaryLighter' />
+									<span>Página Inicial</span>
+								</Button>
 
-              </Link>
-            </li>
+							</Link>
+						</li>
 
-            <li className='group my-[0.2px] group-hover/sidebar:my-[0.5px]'>
-              <Link href={`/`}>
+						<li className='group my-0 group-hover/sidebar:my-[1px]'>
+							<Link href={'/'}>
                 
-                <Button variant='sidebar' className='block group-hover/sidebar:hidden'>
-                  <FaUser className='text-2xl group-hover:text-primary dark:group-hover:text-primaryLighter' />
-                </Button>
+								<Button variant='sidebar' className='block group-hover/sidebar:hidden'>
+									<FaUser className='text-2xl group-hover:text-primary dark:group-hover:text-primaryLighter' />
+								</Button>
 
-                <Button variant='sidebar' className='hidden group-hover/sidebar:flex'>
-                  <FaUser className='text-2xl group-hover:text-primary dark:group-hover:text-primaryLighter' />
-                  <span>Seu Perfil</span>
-                </Button>
+								<Button variant='sidebar' className='hidden group-hover/sidebar:flex'>
+									<FaUser className='text-2xl group-hover:text-primary dark:group-hover:text-primaryLighter' />
+									<span>Seu Perfil</span>
+								</Button>
 
-              </Link>
-            </li>
+							</Link>
+						</li>
             
-            <Separator className='bg-primaryGray' />
+						<Separator className='bg-primaryGray' />
             
-            <li className='group my-[0.1px] group-hover/sidebar:mt-[0.3px]'>
-              <Link href={`/`}>
+						<li className='group my-[1px] group-hover/sidebar:mt-0'>
+							<Link href={'/'}>
                   
-                <Button variant='sidebar' className='block group-hover/sidebar:hidden'>
-                  <ImBooks className='text-2xl group-hover:text-primary dark:group-hover:text-primaryLighter' />
-                </Button>
+								<Button variant='sidebar' className='block group-hover/sidebar:hidden'>
+									<ImBooks className='text-2xl group-hover:text-primary dark:group-hover:text-primaryLighter' />
+								</Button>
 
-                <Button variant='sidebar' className='hidden group-hover/sidebar:flex'>
-                  <ImBooks className='text-2xl group-hover:text-primary dark:group-hover:text-primaryLighter' />
-                  <span>Livros</span>
-                </Button>
+								<Button variant='sidebar' className='hidden group-hover/sidebar:flex'>
+									<ImBooks className='text-2xl group-hover:text-primary dark:group-hover:text-primaryLighter' />
+									<span>Livros</span>
+								</Button>
 
-              </Link>
-            </li>
+							</Link>
+						</li>
             
             
-            <li className='group my-[0.5px] group-hover/sidebar:mt-[0.2px]'>
-              <Link href={`/`}>
+						<li className='group my-[0.5px] group-hover/sidebar:mt-[0.2px]'>
+							<Link href={'/'}>
                 
-                <Button variant='sidebar' className='block group-hover/sidebar:hidden'>
-                  <IoInformationCircleOutline className='text-2xl group-hover:text-primary dark:group-hover:text-primaryLighter' />
-                </Button>
+								<Button variant='sidebar' className='block group-hover/sidebar:hidden'>
+									<IoInformationCircleOutline className='text-2xl group-hover:text-primary dark:group-hover:text-primaryLighter' />
+								</Button>
 
-                <Button variant='sidebar' className='hidden group-hover/sidebar:flex'>
-                  <IoInformationCircleOutline className='text-2xl group-hover:text-primary dark:group-hover:text-primaryLighter' />
-                  <span>Informações</span>
-                </Button>
+								<Button variant='sidebar' className='hidden group-hover/sidebar:flex'>
+									<IoInformationCircleOutline className='text-2xl group-hover:text-primary dark:group-hover:text-primaryLighter' />
+									<span>Informações</span>
+								</Button>
 
-              </Link>
-            </li>
+							</Link>
+						</li>
 
-            <Separator className='bg-primaryGray' />
+						<Separator className='bg-primaryGray' />
 
-          </ul>
+					</ul>
 
-          <ul className='text-gray-500 dark:text-gray-300 font-bold w-14 group-hover/sidebar:w-full flex flex-col gap-4 p-2'>
-            <Separator className='bg-primaryGray' />
+					<ul className='text-gray-500 dark:text-gray-300 font-bold w-14 group-hover/sidebar:w-full flex flex-col gap-4 p-2'>
+						<Separator className='bg-primaryGray' />
             
-            <li className='group my-[0.5px] group-hover/sidebar:my-0'>
-              <Link href={`/`}>
+						<li className='group my-[0.5px] group-hover/sidebar:my-0'>
+							<Link href={'/'}>
 
-                <Button variant='sidebar' className='block group-hover/sidebar:hidden'>
-                  <AiFillSetting className='text-2xl group-hover:text-primary dark:group-hover:text-primaryLighter' />
-                </Button>
+								<Button variant='sidebar' className='block group-hover/sidebar:hidden'>
+									<AiFillSetting className='text-2xl group-hover:text-primary dark:group-hover:text-primaryLighter' />
+								</Button>
 
-                <Button variant='sidebar' className='hidden group-hover/sidebar:flex'>
-                  <AiFillSetting className='text-2xl group-hover:text-primary dark:group-hover:text-primaryLighter' />
-                  <span>Configurações</span>
-                </Button>
+								<Button variant='sidebar' className='hidden group-hover/sidebar:flex'>
+									<AiFillSetting className='text-2xl group-hover:text-primary dark:group-hover:text-primaryLighter' />
+									<span>Configurações</span>
+								</Button>
                 
-              </Link>
-            </li>
-          </ul>
-        </div>
-      </div>
-    </>
-  );
+							</Link>
+						</li>
+					</ul>
+				</div>
+			</div>
+		</>
+	);
 };
 
 export default Sidebar;
