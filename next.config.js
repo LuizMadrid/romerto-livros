4f59c160d47b7ebf6cd792b76f505452f5480811
Luiz Madrid <luizmadrid.dev@gmail.com>
2024-01-15 19:13:09 -0300

    images domains has been updated.

--- next.config.js
+++ next.config.js
@@ -4,6 +4,6 @@ const nextConfig = {};
 module.exports = {
 	reactStrictMode: true,
 	images: {
-		domains: ['covers.openlibrary.org'],
+		domains: ['covers.openlibrary.org', 'avatars.githubusercontent.com', 'https://pixabay.com/vectors/blank-profile-picture-mystery-man-973460/'],
 	},
 };
